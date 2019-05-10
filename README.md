0adea30d8268484d5650376253f786b6b59f3841
Shota Tamura <tamura@yuuniworks.com>
2019-05-10 16:04:36 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# What's this
+
+This is Vue.js component to embed youtube videos to your project RESPONSIVELY.
+
+# how to use
+
+Copy `Youtube.vue` to your project and use it as follows:
+
+```js
+// maxWidth is optional
+<Youtube videoId="8Hgt9HYaCDA" maxWidth="360px" />
+```
+
+That's it! Your video will fit to its parent(100% width)!
---
+++ Youtube.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :style="styles.widthLimitter">
+    <div :style="styles.renderingAreaProvider">
+      <iframe :src="src" :style="styles.iframe" frameborder="0" allowfullscreen="true"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["maxWidth", "videoId"],
+  computed: {
+    src() {
+      return `http://www.youtube.com/embed/${this.videoId}`;
+    }
+  },
+  data() {
+    return {
+      styles: {
+        widthLimitter: {
+          maxWidth: this.maxWidth ? this.maxWidth : null
+        },
+        renderingAreaProvider: {
+          background: "#f0f0f0",
+          height: 0,
+          margin: "1rem 0",
+          /*
+           * - '75%' indicates the aspect rasio (3/4 = 75%).
+           * - note that percentage inside 'padding-(top|bottom)' is calculated based on
+           *   its current width. this is a specification of 'calc' used inside
+           *   the 'padding-(top|bottom)' property.
+           *
+           * see: https://nathan.gs/2018/01/07/responsive-slideshare-iframe/
+           */
+          paddingBottom: "calc(56.25%)",
+          position: "relative",
+          width: "100%"
+        },
+        iframe: {
+          height: "100%",
+          left: 0,
+          position: "absolute",
+          top: 0,
+          width: "100%"
+        }
+      }
+    };
+  }
+};
+</script>
+
